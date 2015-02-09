801e2b60b2514e8f737a8105805de49843c96a7c
gee19 <gee19haine@gmail.com>
2015-02-08 23:25:02 -0500

    seed data

--- HospiceNiagara/Migrations/Configuration.cs
+++ HospiceNiagara/Migrations/Configuration.cs
@@ -1,9 +1,14 @@
 namespace HospiceNiagara.Migrations
 {
+    using HospiceNiagara.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Data.Entity.Validation;
     using System.Linq;
+    using System.Text;
 
     internal sealed class Configuration : DbMigrationsConfiguration<HospiceNiagara.Models.ApplicationDbContext>
     {
@@ -27,6 +32,136 @@ namespace HospiceNiagara.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            //create Role admin if it does not exist
+            if (!context.Roles.Any(r => r.Name == "Admin"))
+            {
+                var roleresult = roleManager.Create(new IdentityRole("Admin"));
+            }
+
+            //create role Board
+            if (!context.Roles.Any(r => r.Name == "Board"))
+            {
+                var roleresult = roleManager.Create(new IdentityRole("Board"));
+            }
+
+            //create role Staff
+            if (!context.Roles.Any(r => r.Name == "Staff"))
+            {
+                var roleresult = roleManager.Create(new IdentityRole("Staff"));
+            }
+
+            //create role volunteer
+            if (!context.Roles.Any(r => r.Name == "Volunteer"))
+            {
+                var roleresult = roleManager.Create(new IdentityRole("Volunteer"));
+            }
+
+
+            //Create a generic user
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            //Create admin user
+            var adminUser = new ApplicationUser
+            {
+                UserName = "admin@outlook.com",
+                Email = "admin@outlook.com",
+                FirstName = "Amanda",
+                LastName = "Cage",
+                PhoneNumber = "905-341-3222",
+                DOB = new DateTime(1989, 04, 22)
+            };
+
+
+            //Create Staff
+            var staffUser = new ApplicationUser
+            {
+                UserName = "staff@outlook.com",
+                Email = "staff@outlook.com",
+                FirstName = "Jay",
+                LastName = "Swanson",
+                PhoneNumber = "905-341-3222",
+                DOB = new DateTime(1990, 05, 23)
+            };
+
+            //Create board user
+            var boardUser = new ApplicationUser
+            {
+                UserName = "board@outlook.com",
+                Email = "board@outlook.com",
+                FirstName = "Board",
+                LastName = "Meele",
+                PhoneNumber = "905-341-3222",
+                DOB = DateTime.Today
+            };
+
+            //Create Volunteer
+            var volunteerUser = new ApplicationUser
+            {
+                UserName = "volunteer@outlook.com",
+                Email = "volunteer@outlook.com",
+                FirstName = "Erika",
+                LastName = "Meele",
+                PhoneNumber = "905-341-3222",
+                DOB = DateTime.Today
+            };
+
+
+            //Create admin account account and add to admin role
+            if (!context.Users.Any(u => u.Email == adminUser.UserName))
+            {
+                manager.Create(adminUser, "password");
+                manager.AddToRole(adminUser.Id, "Admin");
+            }
+
+            //Create Staff account and add to staff role
+            if (!context.Users.Any(u => u.Email == staffUser.UserName))
+            {
+                manager.Create(staffUser, "password");
+                manager.AddToRole(staffUser.Id, "Staff");
+            }
+
+            //create board user and add to board role
+            if (!context.Users.Any(u => u.Email == boardUser.UserName))
+            {
+                manager.Create(boardUser, "password");
+                manager.AddToRole(boardUser.Id, "Board");
+            }
+
+            //create volunteer user and add to Vounteer role
+            if (!context.Users.Any(u => u.Email == volunteerUser.UserName))
+            {
+                manager.Create(volunteerUser, "password");
+                manager.AddToRole(volunteerUser.Id, "Volunteer");
+            }
+        }
+        private void SaveChanges(DbContext context)
+        {
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var failure in ex.EntityValidationErrors)
+                {
+                    sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+                    foreach (var error in failure.ValidationErrors)
+                    {
+                        sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+                        sb.AppendLine();
+                    }
+                }
+
+                throw new DbEntityValidationException(
+                    "Entity Validation Failed - errors follow:\n" +
+                    sb.ToString(), ex
+                ); // Add the original exception as the innerException
+            }
         }
     }
 }
