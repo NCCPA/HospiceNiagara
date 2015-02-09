24a3fe50d94fd8e66d4fb26f5f1ce934dd8974f0
lunchboxsuhi <morozoandrei@gmail.com>
2015-02-08 23:57:33 -0500

    updates calendar

--- HospiceNiagara/Scripts/custom-sidebar.js
+++ HospiceNiagara/Scripts/custom-sidebar.js
@@ -27,11 +27,66 @@ $(document).ready(function () {
     $('a[href="' + this.location.pathname + '"]').parent().addClass('active');
 
 
-    //FullCalendar JS and Momentom.js
+    var date = new Date();
+    var d = date.getDate();
+    var m = date.getMonth();
+    var y = date.getFullYear();
+  
     $('#calendar').fullCalendar({
-        // put your options and callbacks here
-    })
-
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay'
+        },
+        editable: true,
+        events: [
+          {
+              title: 'All Day Event',
+              start: new Date(y, m, 1)
+          },
+          {
+              title: 'Long Event',
+              start: new Date(y, m, d-5),
+              end: new Date(y, m, d-2)
+          },
+          {
+              id: 999,
+              title: 'Repeating Event',
+              start: new Date(y, m, d-3, 16, 0),
+              allDay: false
+          },
+          {
+              id: 999,
+              title: 'Repeating Event',
+              start: new Date(y, m, d+4, 16, 0),
+              allDay: false
+          },
+          {
+              title: 'Meeting',
+              start: new Date(y, m, d, 10, 30),
+              allDay: false
+          },
+          {
+              title: 'Lunch',
+              start: new Date(y, m, d, 12, 0),
+              end: new Date(y, m, d, 14, 0),
+              allDay: false
+          },
+          {
+              title: 'Birthday Party',
+              start: new Date(y, m, d+1, 19, 0),
+              end: new Date(y, m, d+1, 22, 30),
+              allDay: false
+          },
+          {
+              title: 'Click for Google',
+              start: new Date(y, m, 28),
+              end: new Date(y, m, 29),
+              url: 'http://google.com/'
+          }
+        ],
+        eventColor: '#378006'
+    });
 
 
     ////make sidebar big as viewport or devicing viewing
