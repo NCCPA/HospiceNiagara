f7b9ad08fb7f3c9fdc9e1b9955638e1d206a87da
havoux <donnygovan4@gmail.com>
2015-03-17 17:01:11 -0400

    contact_viewmodel

---
+++ HospiceNiagara/Models/ViewModels/ContactsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HospiceNiagara.Models.ViewModels
+{
+    public class ContactsViewModel
+    {
+    }
+}
