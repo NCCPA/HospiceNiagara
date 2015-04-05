069a0a13cb308e1a9674cc1f6132d1caa26964e2
lunchboxsuhi <morozoandrei@gmail.com>
2015-04-05 19:30:30 -0400

    file changes

--- HospiceNiagara/Controllers/FileController.cs
+++ HospiceNiagara/Controllers/FileController.cs
@@ -1,9 +1,12 @@
 ﻿using HospiceNiagara.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -72,7 +75,7 @@ namespace HospiceNiagara.Controllers
                 };
 
                 db.Files.Add(newFile);
-                db.SaveChanges();
+                SaveChanges(db);
             }
             return RedirectToAction("Index");
         }
@@ -126,6 +129,31 @@ namespace HospiceNiagara.Controllers
             base.Dispose(disposing);
         }
 
+        private void SaveChanges(DbContext context)
+        {
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                StringBuilder sb = new StringBuilder();
 
+                foreach (var failure in ex.EntityValidationErrors)
+                {
+                    sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+                    foreach (var error in failure.ValidationErrors)
+                    {
+                        sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+                        sb.AppendLine();
+                    }
+                }
+
+                throw new DbEntityValidationException(
+                    "Entity Validation Failed - errors follow:\n" +
+                    sb.ToString(), ex
+                ); // Add the original exception as the innerException
+            }
+        }
     }
 }
